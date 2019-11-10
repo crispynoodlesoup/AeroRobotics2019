7226853de3802917bfdc5c7fb63f70095ff62437
crispynoodlesoup <51384880+crispynoodlesoup@users.noreply.github.com>
2019-11-10 14:37:54 -0600

    Update and rename Macanumbot.txt to Macanumbot.java

--- TeamCode/Macanumbot.java
+++ TeamCode/Macanumbot.java
@@ -7,21 +7,24 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
 /**
- * 
+ * TeleOp for mechanumbot with CRServo and intake system
  */
 
 
 @TeleOp(name="Mecanumbot", group="Linear Opmode")
 public class Mecanum_Linear extends LinearOpMode {
 
-    // Declare OpMode members.
+    // Declare runtime
     private ElapsedTime runtime = new ElapsedTime();
+    
+    //Declare hardware variables
     private DcMotor leftFront   = null;
     private DcMotor rightFront  = null;
     private DcMotor leftRear    = null;
     private DcMotor rightRear   = null;
     private DcMotor intakeLeft  = null;
     private DcMotor intakeRight = null;
+    private CRServo servoArm    = null;
 
     @Override
     public void runOpMode() {
@@ -30,13 +33,13 @@ public class Mecanum_Linear extends LinearOpMode {
 
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
         leftFront   = hardwareMap.get(DcMotor.class, "left_front");
         rightFront  = hardwareMap.get(DcMotor.class, "right_front");
         leftRear    = hardwareMap.get(DcMotor.class, "left_rear");
         rightRear   = hardwareMap.get(DcMotor.class, "right_rear");
         intakeLeft  = hardwareMap.get(DcMotor.class, "intake_left");
         intakeRight = hardwareMap.get(DcMotor.class, "intake_right");
+        servoArm    = hardwareMap.get(CRServo.class, "servoArm");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
@@ -50,39 +53,62 @@ public class Mecanum_Linear extends LinearOpMode {
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
         runtime.reset();
+        
+        boolean sneak       = false;
+        boolean toggleSneak = false;
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             
-            // Setup a variable for each drive wheel to save power level for telemetry
+            // Setup a variable for variables to determine mecanum spin
             double forward = -gamepad1.left_stick_y;
             double turn = gamepad1.right_stick_x;
             double strafe = gamepad1.left_stick_x;
-            boolean succ = gamepad1.left_bumper;
-            boolean unsucc = gamepad1.right_bumper;
+            boolean sneakPrev = sneak;
+            sneak = gamepad1.left_stick_button;
+            double varSneak = gamepad1.right_trigger;
 
+            //math for mecanum wheels
             double leftF = forward + turn + strafe;
             double rightF = forward - turn - strafe;
             double leftR = forward + turn - strafe;
             double rightR = forward - turn + strafe;
         
-            leftF = Range.clip(leftF, -1, 1);
-            rightF = Range.clip(rightF, -1, 1);
-            leftR = Range.clip(leftR, -1, 1);
-            rightR = Range.clip(rightR, -1, 1);
+            //logic for Sneaking
+            varSneak = Range.clip(varSneak, 0, 0.85);
+            if(sneak && !sneakPrev)
+                toggleSneak = !toggleSneak;
+            if(toggleSneak)
+                leftF = Range.clip(leftF, -0.4, 0.4);
+                rightF = Range.clip(rightF, -0.4, 0.4);
+                leftR = Range.clip(leftR, -0.4, 0.4);
+                rightR = Range.clip(rightR, -0.4, 0.4);
+            } else {
+                leftF = Range.clip(leftF, -1 + varSneak, 1 - varSneak);
+                rightF = Range.clip(rightF, -1 + varSneak, 1 - varSneak);
+                leftR = Range.clip(leftR, -1 + varSneak, 1 - varSneak);
+                rightR = Range.clip(rightR, -1 + varSneak, 1 - varSneak);
+            }
             
+            //set power for mecanum wheels
             leftFront.setPower(leftF);
             rightFront.setPower(rightF);
             leftRear.setPower(leftR);
             rightRear.setPower(rightR);
+            
+            //variables for intake
+            boolean succ = gamepad1.left_bumper;
+            boolean unsucc = gamepad1.right_bumper;
+            
+            //logic for intake system
             if(succ && !unsucc) {
-                intakeLeft.setPower(1.0);
-                intakeRight.setPower(1.0);
+                intakeLeft.setPower(0.6);
+                intakeRight.setPower(0.6);
             } else if(unsucc && !succ) {
                 intakeLeft.setPower(-1.0);
                 intakeRight.setPower(-1.0);
             } else {
-                in takeLeft.setPower(0.0);
+                intakeLeft.setPower(0.0);
                 intakeRight.setPower(0.0);
             }
 
