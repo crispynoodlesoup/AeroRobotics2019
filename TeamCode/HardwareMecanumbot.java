59f82c2e31b1ca1b5595b297bf86b789cef30d5f
crispynoodlesoup <51384880+crispynoodlesoup@users.noreply.github.com>
2019-11-14 21:43:03 -0600

    Update HardwareMecanumbot.java

--- TeamCode/HardwareMecanumbot.java
+++ TeamCode/HardwareMecanumbot.java
@@ -15,7 +15,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 
-public class HardwareTestbot
+public class HardwareMecanumbot
 {
     //define opmode members
     private LinearOpMode myOpMode;
@@ -37,20 +37,23 @@ public class HardwareTestbot
     // motor for arm
     public Servo   servoArm = null;
     public DcMotor Arm     = null;
+    
+    //variables
+    private double leftF;
+    private double rightF;
+    private double leftR;
+    private double rightR;
 
-    public HardwareTestbot(){
+    public HardwareMecanumbot(){
     }
     
     /* Initialize standard Hardware interfaces */
     public void initDrive(LinearOpMode opMode) {
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-        
         myOpMode = opMode;
 
         //encoderState("off");
-        encoderState("run");
-        encoderState("reset");
+        //encoderState("run");
+        //encoderState("reset");
 
         //parameters we're sending to the imu
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
@@ -63,16 +66,16 @@ public class HardwareTestbot
         
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
-        leftFront   = hardwareMap.get(DcMotor.class, "left_front");
-        rightFront  = hardwareMap.get(DcMotor.class, "right_front");
-        leftRear    = hardwareMap.get(DcMotor.class, "left_rear");
-        rightRear   = hardwareMap.get(DcMotor.class, "right_rear");
-        intakeLeft  = hardwareMap.get(DcMotor.class, "intake_left");
-        intakeRight = hardwareMap.get(DcMotor.class, "intake_right");
-        servoArm    = hardwareMap.get(Servo.class,   "servoArm");
+        leftFront   = myOpMode.hardwareMap.get(DcMotor.class, "left_front");
+        rightFront  = myOpMode.hardwareMap.get(DcMotor.class, "right_front");
+        leftRear    = myOpMode.hardwareMap.get(DcMotor.class, "left_rear");
+        rightRear   = myOpMode.hardwareMap.get(DcMotor.class, "right_rear");
+        intakeLeft  = myOpMode.hardwareMap.get(DcMotor.class, "intake_left");
+        intakeRight = myOpMode.hardwareMap.get(DcMotor.class, "intake_right");
+        servoArm    = myOpMode.hardwareMap.get(Servo.class,   "servoArm");
         
         //init imu
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu = myOpMode.hardwareMap.get(BNO055IMU.class, "imu");
         imu.initialize(parameters);
         
         // Most robots need the motor on one side to be reversed to drive forward
@@ -83,6 +86,17 @@ public class HardwareTestbot
         rightRear.setDirection(DcMotor.Direction.REVERSE);
         intakeLeft.setDirection(DcMotor.Direction.FORWARD);
         intakeRight.setDirection(DcMotor.Direction.REVERSE);
+        
+        //run using encoders
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        
+            leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            leftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         
         //brake the motors
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -134,25 +148,25 @@ public class HardwareTestbot
     }
     public void encoderState(String a){
         if(a.equals("reset")){
-            FLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            FRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            RLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            RRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            leftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         }else if( a.equals("run")){
-            FLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            FRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            RLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            RRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            leftRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }else if(a.equals("position")){
-            FLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            FRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            RLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            RRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         }else if(a.equals("off")){
-            FLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            FRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            RLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            RRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            leftRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            rightRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         }
     }
-}   
+} 
