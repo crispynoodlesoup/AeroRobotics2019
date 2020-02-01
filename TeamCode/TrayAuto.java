10b613828e5b0fdd2b0f8f650cbb544a57027d30
crispynoodlesoup <51384880+crispynoodlesoup@users.noreply.github.com>
2020-01-31 23:16:57 -0600

    Update TrayAuto.java

--- TeamCode/TrayAuto.java
+++ TeamCode/TrayAuto.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
-import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -14,20 +13,26 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-/*
-@Autonomous(name="Tray", group="generated")
+
+@Autonomous(name="TrayAuto", group="generated")
 public class TrayAuto extends LinearOpMode {
-        //28 * 20 / (2ppi * 4.125)
-        Double width = 18.0; //inches
-        Double cpr = 383.6; //counts per rotation
-        Double gearratio = 13.7;
-        Double diameter = 3.937;
-        Double cpi = (cpr * gearratio)/(Math.PI * diameter); //counts per inch, 28cpr * gear ratio / (2 * pi * diameter (in inches, in the center))
-        Double bias = 0.205;
-        Double meccyBias = 0.21;//change to adjust only strafing movement, 0.04576
-        Double conversion = cpi * bias;
+        //width = 18.0; //inches
+        //cpr = 383.6; //counts per rotation
+        //gearratio = 13.7;
+        //diameter = 3.937;
+        //cpi = (cpr * gearratio)/(Math.PI * diameter); //counts per inch, cpr * gear ratio / (2 * pi * diameter)
+        //bias = 0.205;
+        int conversion = 88;
+        
+        // meccyBias = 0.21;//change to adjust only strafing movement
+        int straifConv = 89;
+        
+        int colorOffset  = 0;
+        
+        int r; 
+        
         Boolean exit = false;
-        double servoPos = 0.0;
+        double redSide = -1.0;
         ElapsedTime runtime = new ElapsedTime();
         GyroStuff gyro = new GyroStuff();
         HardwareMecanumbot robot = new HardwareMecanumbot();
@@ -36,31 +41,26 @@ public class TrayAuto extends LinearOpMode {
                 robot.initDrive(this);
                 gyro.initDrive(robot);
                 
-                robot.servoArm.setPosition(0);
-                
+                //robot.liftPos(17000);
+                //sleep(500);
+                robot.servoGrab1.setPosition(1);
+                robot.servoGrab2.setPosition(0.6);
+                robot.servoArm.setPosition(0.35);
+                robot.servoTray1.setPosition(0.7);
+                robot.servoTray2.setPosition(0.6);
+                //
                 waitForStart();
-                //
-                strafeToPosition(-30.5, 0.35);
-                //
-                robot.servoArm.setPosition(1);
-                sleep(1000);
-                servoPos = 1.0;
-                //
-                strafeToPosition(15, 0.15);
-                //
-                turnWithEncoder(180, 0.5);
-                //
-                //robot.servoArm.setPosition(0);
-                //sleep(1000);
-                //servoPos = 0.0;
-                //moveToPosition(-20.0*redSide, 0.5);
-                turnWithEncoder(90, 0.5);
-                robot.servoArm.setPosition(0.5);
-                //
+                moveToPostition(32.0, 1.0);
+                robot.eatTray(true);
+                driveArc(-90, 0.5);
+                robot.eatTray(false);
+                moveToPostition(4.0, 1.0);
+                turnToTarget(179);
+                robot.liftPos(100);
                 }
         public void moveToPosition(double inches, double speed){
                 //
-                int move = (int)(Math.round(inches*conversion));
+                int move = (int)(Math.round(inches * conversion));
                 //
                 robot.leftFront.setTargetPosition(robot.leftFront.getCurrentPosition() + move);
                 robot.rightFront.setTargetPosition(robot.rightFront.getCurrentPosition() + move);
@@ -72,29 +72,31 @@ public class TrayAuto extends LinearOpMode {
                 robot.leftRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 robot.rightRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 //
-                robot.leftFront.setPower(speed);
-                robot.rightFront.setPower(speed);
-                robot.leftRear.setPower(speed);
-                robot.rightRear.setPower(speed);
+                if(inches < 0)
+                        r = -1;
+                else
+                        r = 1;
                 //
-                while (robot.leftRear.isBusy() && robot.leftFront.isBusy() && robot.rightFront.isBusy() && robot.rightRear.isBusy()){
-                if (exit) {
+                driveStraight(0,speed,r);
+                //
+                while (robot.leftRear.isBusy() || robot.leftFront.isBusy() || robot.rightFront.isBusy() || robot.rightRear.isBusy()){
+                        speed *= 0.954 + (inches * 0.0008);
+                        speed = Range.clip(speed, 0.05, 0.999999);
+                        driveStraight(0,speed,r);
+                        telemetry.addData("pidcalc", "%.3f", gyro.calcPID(0));
+                        telemetry.addData("red", "%d", robot.color.red());
+                        telemetry.addData("green", "%d", robot.color.green());
+                        telemetry.addData("blue", "%d", robot.color.blue());
+                        telemetry.update();
+                }
                 robot.leftFront.setPower(0);
                 robot.rightFront.setPower(0);
                 robot.leftRear.setPower(0);
                 robot.rightRear.setPower(0);
-                return;
-                }
-                }
-                robot.leftFront.setPower(0);
-                robot.rightFront.setPower(0);
-                robot.leftRear.setPower(0);
-                robot.rightRear.setPower(0);
-                return;
                 }
         public void turnWithGyro(double degrees,double time){
-                runtime.reset();
-                while(gyro.getError(degrees)>2 || runtime.seconds() > time){
+                runtime.reset(); 
+                /*while(gyro.getError(degrees)>2 || runtime.seconds() > time){
                         double spin = gyro.calcPID(degrees);
 
                         // normalize all motor speeds so no values exceeds 100%.
@@ -105,11 +107,11 @@ public class TrayAuto extends LinearOpMode {
                         robot.rightFront.setPower(spin);
                         robot.leftRear.setPower(spin);
                         robot.rightRear.setPower(spin);
-                }
+                }*/
         }
         public void strafeToPosition(double inches, double speed){
                 //
-                int move = (int)(Math.round(inches * cpi * meccyBias));
+                int move = (int)(Math.round(inches * straifConv));
                 //
                 robot.leftFront.setTargetPosition(robot.leftFront.getCurrentPosition() + move);
                 robot.rightFront.setTargetPosition(robot.rightFront.getCurrentPosition() - move);
@@ -121,45 +123,53 @@ public class TrayAuto extends LinearOpMode {
                 robot.leftRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 robot.rightRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 //
-                robot.leftFront.setPower(speed);
-                robot.rightFront.setPower(speed);
-                robot.leftRear.setPower(speed);
-                robot.rightRear.setPower(speed);
+                if(inches < 0)
+                        r = -1;
+                else
+                        r = 1;
                 //
-                while (robot.leftRear.isBusy() && robot.rightFront.isBusy() && robot.leftFront.isBusy() && robot.rightRear.isBusy()){
-                        robot.servoArm.setPosition(servoPos);
+                straifStraight(0, speed, r);
+                //
+                while (robot.leftRear.isBusy() || robot.rightFront.isBusy() || robot.leftFront.isBusy() || robot.rightRear.isBusy()){
+                        speed *= 0.954 + (inches * 0.0008);
+                        speed = Range.clip(speed, 0.05, 0.999999);
+                        straifStraight(0, speed, r);
+                        telemetry.addData("pidcalc", "%.3f", gyro.calcPID(0));
+                        telemetry.update();
                 }
                 robot.leftFront.setPower(0);
                 robot.rightFront.setPower(0);
                 robot.leftRear.setPower(0);
                 robot.rightRear.setPower(0);
-                return;
         }
-        public void turnWithEncoder(double degrees, double input){
-                int turn = (int)(Math.round(degrees/19.0 * cpi));
-                //
-                robot.leftFront.setTargetPosition(robot.leftFront.getCurrentPosition() + turn);
-                robot.rightFront.setTargetPosition(robot.rightFront.getCurrentPosition() - turn);
-                robot.leftRear.setTargetPosition(robot.leftRear.getCurrentPosition() + turn);
-                robot.rightRear.setTargetPosition(robot.rightRear.getCurrentPosition() - turn);
-                //
-                robot.leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                robot.rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                robot.leftRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                robot.rightRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                //
-                robot.leftFront.setPower(input);
-                robot.rightFront.setPower(input);
-                robot.leftRear.setPower(input);
-                robot.rightRear.setPower(input);
-                
-                while (robot.leftRear.isBusy() && robot.rightFront.isBusy() && robot.leftFront.isBusy() && robot.rightRear.isBusy()){
-                        robot.servoArm.setPosition(servoPos);
+        public void liftPos(int pos) {
+                robot.lift.setTargetPosition(pos);
+                robot.lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                robot.lift.setPower(1);
+                while(robot.lift.isBusy()){}
+        }
+        public void turnToTarget(double target) {
+                while (gyro.calcPID(0) > 0.1){
+                robot.moveLateral(0, -gyro.calcPID(0), 0, 0);
+                telemetry.addData("pidcalc", "%.3f", gyro.calcPID(0));
+                telemetry.addData("red", "%d", robot.color.red());
+                telemetry.addData("green", "%d", robot.color.green());
+                telemetry.addData("blue", "%d", robot.color.blue());
+                telemetry.update();
                 }
-                robot.leftFront.setPower(0);
-                robot.rightFront.setPower(0);
-                robot.leftRear.setPower(0);
-                robot.rightRear.setPower(0);
-                return;
         }
-}*/
+        public void driveArc(double target, double TurnSpeed) {
+                while(Math.abs(gyro.calcPID(Target) > 0.12) {
+                        robot.moveLateral(1, TurnSpeed*2, 0, 0);
+                }
+                TurnToTarget(target);
+        }
+        public void driveStraight(double target, double speed, int reverse) {
+                robot.moveLateral(speed, -gyro.calcPID(target)*reverse, 0, 0);
+        }
+        public void straifStraight(double target, double speed, int reverse) {
+                robot.moveLateral(0, gyro.calcPID(target)*reverse, speed, 0);
+        }
+}
+
+
